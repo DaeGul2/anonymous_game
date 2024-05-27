f031473aa5082e84056c78b3502427645be288fd
DaeGul2 <alsxogml1234@gmail.com>
2024-05-27 18:51:13 +0900

    Update Owner.js

--- client/src/pages/stages/Owner.js
+++ client/src/pages/stages/Owner.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import socket from '../../socket';
 
-function Owner() {
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+function Owner({ roomId }) {
+  const changeStage = async () => {
+    try {
+      await axiosInstance.post(`/api/rooms/changeStage/${roomId}`);
+      socket.emit('stageChanged', roomId);
+    } catch (error) {
+      console.error('Error changing stage:', error.response.data);
+    }
+  };
+
   return (
-    <div>Owner</div>
-  )
+    <div>
+      <h3>Owner Controls</h3>
+      <button className="btn btn-primary" onClick={changeStage}>Next Stage</button>
+    </div>
+  );
 }
 
-export default Owner
+export default Owner;
